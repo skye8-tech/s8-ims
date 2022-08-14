788d484dd08236e6fa8c92ef3a0332896cdf65b7
mulfranck <mulfranck5@gmail.com>
2022-08-14 14:44:01 +0100

    feat: add the main report section

--- src/script.js
+++ src/script.js
@@ -1,2 +1,12 @@
 'use strict'
 
+const $weekBtn = document.querySelector('.id-archive');
+let $currentWeek = document.querySelector('.id-active');
+
+const clickWeekHandler = (e) => {
+    e.target.classList.toggle('id-active');
+    $currentWeek.classList.toggle('id-active');
+    $currentWeek = e.target; // keep track of the current element.
+}
+
+$weekBtn.addEventListener('click', e => {clickWeekHandler(e)})
