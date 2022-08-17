5e2d13d63571019da288b4a773ed7ef86ac7b523
mulfranck <mulfranck5@gmail.com>
2022-08-17 02:46:39 +0100

    add js week switch

--- src/script.js
+++ src/script.js
@@ -2,10 +2,12 @@
 
 const $weekBtn = document.querySelector('.id-archive');
 let $currentWeek = document.querySelector('.id-active');
+let $weekTitle = document.querySelector('#Week-title');
 
 const clickWeekHandler = (e) => {
     e.target.classList.toggle('id-active');
     $currentWeek.classList.toggle('id-active');
+    $weekTitle.textContent = e.target.textContent;
     $currentWeek = e.target; // keep track of the current element.
 }
 
