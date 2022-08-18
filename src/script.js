3a752b199c36445b34c4b2923e62189fbd705240
Ngum <ngumbutlar43@gmail.com>
2022-08-18 07:34:22 +0100

    Feat : Added function to check input values and display an error/success message

--- src/script.js
+++ src/script.js
@@ -7,3 +7,36 @@ const duration = document.getElementById('duration');
 const policy = document.getElementById('check');
 const popup = document.getElementById('modal');
 
+ //Event...#Pop-up message
+
+FormData.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    checkInput();
+
+});
+function checkInput(){
+    //get values from input
+   const schoolValue = school.value.trim();
+   const departmentValue = department.value.trim();
+   const attatchmentValue = attatchment.value.trim();
+   const dateValue = date.value;
+   const policyValue = policy.value;
+
+   if(schoolValue == ''){
+    //show error
+    setErrorfor(school, 'name of school cannot be blank')
+    
+   }else{
+    //show success
+    setSuccessfor(school)
+   }
+};
+function setErrorfor(input, message){
+
+}
+function setSuccessfor(input, message){
+    
+}
+
+
