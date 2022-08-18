e64a25b7a51ada5f68d88796272da6411274dab4
Ngum <ngumbutlar43@gmail.com>
2022-08-18 15:58:13 +0100

    Feat: Modified script.js

---
+++ src/script.js
@@ -0,0 +1,9 @@
+// Disable form submissions if there are invalid fields
+
+const regform = document.getElementById('regform')
+console.log(regform)
+regform.addEventListener('submit', function(){
+    console.log('submitting')
+})
+
+function sum(a, b){}
