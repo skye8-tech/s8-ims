97d824da55f690715c7edfbc932b1b7b1296c09c
Ngum <ngumbutlar43@gmail.com>
2022-08-18 07:32:06 +0100

    Feat: Created the target elements needed for the form validation

--- src/script.js
+++ src/script.js
@@ -1,3 +1,9 @@
 'use strict'
-
+const school = document.getElementById('school');
+const department = document.getElementById('department');
+const attatchment = document.getElementById('attatchment');
+const date = document.getElementById('date');
+const duration = document.getElementById('duration');
+const policy = document.getElementById('check');
+const popup = document.getElementById('modal');
 
