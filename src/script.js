e464d28eddc0c69062f13f74ac1e2bfb8cf8640c
Butlar <97985460+Ngumbutlar@users.noreply.github.com>
2022-08-20 00:55:24 +0100

    Required info validation (#38)
    * Feat: added form validation
    * add js modal trigger

--- src/script.js
+++ src/script.js
@@ -1,2 +1,10 @@
-'use strict'
 
+
+    // Disable form submissions if there are invalid fields
+const regform = document.getElementById('regform')
+regform.addEventListener('submit', function(e){
+    let myModal = new bootstrap.Modal(document.getElementById("staticBackdrop"), {});
+    e.preventDefault();
+    myModal.toggle();
+    
+})
