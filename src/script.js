559a7f0eae70092aa6039927014aed2dbc8d16af
Ngum <ngumbutlar43@gmail.com>
2022-08-23 11:25:46 +0100

    .........Modifications......

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 
 
-    // Disable form submissions if there are invalid fields
+    // Modal activation after validation
 const regform = document.getElementById('regform')
 regform.addEventListener('submit', function(e){
     let myModal = new bootstrap.Modal(document.getElementById("staticBackdrop"), {});
