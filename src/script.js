2fa8098c12c4c1d030d80a04ddab7342e9d2adaa
mulfranck <mulfranck5@gmail.com>
2022-08-24 16:47:49 +0100

    add admin details and script

--- src/script.js
+++ src/script.js
@@ -1,2 +1,14 @@
 'use strict'
 
+const $weekBtn = document.querySelector('.id-archive');
+let $currentWeek = document.querySelector('.id-active');
+let $weekTitle = document.querySelector('#Week-title');
+
+const clickWeekHandler = (e) => {
+    e.target.classList.toggle('id-active');
+    $currentWeek.classList.toggle('id-active');
+    $weekTitle.textContent = e.target.textContent;
+    $currentWeek = e.target; // keep track of the current element.
+}
+
+$weekBtn.addEventListener('click', e => {clickWeekHandler(e)})
