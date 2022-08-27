06e328bc51cb4e9fbb8086631550d921230fe5d4
Nji Franclin Muluh <mulfranck5@gmail.com>
2022-08-27 09:03:04 +0100

    Routing interns pages (#66)
    * chore: add testing folder
    
    * refact: refactor the application to container both forms on the single page
    
    * refact: interns pages for routing
    
    * refact utilise

---
+++ src/js/application.js
@@ -0,0 +1,56 @@
+import { postRequest } from "../utils.js";
+
+const $nextFormBtn = document.querySelector('#next-form-btn');
+const $prevFormBtn = document.querySelector('#prev-form-btn');
+
+const $prevFormContent = document.querySelector('#prev-form');
+const $nextFormContent = document.querySelector('#next-form');
+
+const $formPersonalInfo = document.querySelector('#personal-info');
+const $formRequiredInfo = document.querySelector('#required-info');
+
+const formData = {};
+
+const saveForm1Data = () => {
+    console.log('naga in form 1')
+    formData['firstname'] = $formPersonalInfo.firstname.value;
+    formData['lastname'] = $formPersonalInfo.lastname.value;
+    formData['address'] = $formPersonalInfo.address.value;
+    formData['email'] = $formPersonalInfo.email.value;
+    formData['contact'] = $formPersonalInfo.contact.value;
+    formData['gender'] = $formPersonalInfo.gender.value;
+}
+
+const saveForm2Data = () => {
+    console.log('niga in form 2')
+    formData['schoolname'] = $formRequiredInfo.schoolname.value;
+    formData['department'] = $formRequiredInfo.department.value;
+    formData['special'] = $formRequiredInfo.specialisation.value;
+    formData['startDate'] = $formRequiredInfo.startDate.value;
+    formData['startDate'] = $formRequiredInfo.attachment.value;
+    formData['startDate'] = $formRequiredInfo.level.value;
+}
+
+
+
+const formToggler = () => {
+    $prevFormContent.classList.toggle('invisible');
+    $nextFormContent.classList.toggle('invisible');
+}
+
+$nextFormBtn.addEventListener('click', () => {
+    saveForm1Data();
+    formToggler();
+})
+$prevFormBtn.addEventListener('click', formToggler)
+
+$formRequiredInfo.addEventListener('submit', (e) => {
+    e.preventDefault();
+    saveForm2Data();
+
+    console.log(formData)
+    postRequest('applications', formData)
+        .then(res => {
+            console.log(res);
+        })
+})
