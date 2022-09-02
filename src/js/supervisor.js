ab76f8e11a837db588e16787a0e50acfa843bedc
mulfranck <mulfranck5@gmail.com>
2022-09-02 20:32:51 +0100

    feat: populating the dom

--- src/js/supervisor.js
+++ src/js/supervisor.js
@@ -1,3 +1,35 @@
-import { logout } from "../utils.js";
+import { getRequest, logout } from "../utils.js";
 
-document.querySelector('[data-logout]').addEventListener('click', logout);
+
+const internTemplate = document.querySelector('#intern-template');
+const $internList = document.querySelector('[data-intern-list]');
+
+function populate(datat) {
+    const internDiv = internTemplate.content.cloneNode(true);
+    const name = internDiv.querySelector('[data-intern-name]');
+    const img = internDiv.querySelector('[data-intern-img]');
+
+
+    name.textContent = datat.name;
+    img.src = "https://picsum.photos/200";
+
+
+    $internList.append(internDiv);
+
+
+}
+
+getRequest('users')
+    .then(data => {
+        console.log(data)
+        data.filter(data => data.role == 'user').forEach(dt => {
+            populate(dt);
+        })
+    }).catch(err => {
+
+    })
+
+
+
+
+document.querySelector('#data-logout').addEventListener('click', logout);
