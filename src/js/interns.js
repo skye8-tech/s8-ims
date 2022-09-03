8374f4ae3e38e1a42cdd9629719d8bb914c7dfd8
Ngum <ngumbutlar43@gmail.com>
2022-09-03 14:20:35 +0100

    Added userid to logbook(formdata) contents

--- src/js/interns.js
+++ src/js/interns.js
@@ -1,65 +1,35 @@
 'use strict'
-
+//import postRequest function from utils.js
 import { postRequest } from "../utils.js";
 
-const $save = document.querySelector('#save');
-//const $submit = document.querySelector('#submit');
-
+// Declaration of variables
+let id = JSON.parse(sessionStorage.getItem('user_details')).id;
 const $logbook = document.querySelector('#form1');
-//giconst $review = document.querySelector('#form2');
 
+//New form data to hold users input data from logbook
 const form1data = {};
 
+//Saveform1Data function saves all user input in a particular format
 const saveform1Data = () => {
-    //form1data['userid'] = $logbook.userid.value,
+    form1data['userid']= id,
     form1data['content'] = $logbook.content.value,
-    form1data['date'] = $logbook.date.value;
-
-   
+    form1data['date'] = $logbook.date.value; 
 }
-// const form2data = {};
-
-// const saveform2Data = ()=>{
-//     //form2data['userid'] = $review.userid.value,
-//     form2data['content1'] = $review.content1.value;
-//     //form2data['date'] = $review.date.value;
-
-   
-// }
-
-$save.addEventListener('click', () => {
-    saveform1Data();
-
-})
-// $submit.addEventListener('click', () => {
-//     saveform2Data();
-
-// })
+// Method to save form data and poston end point
 $logbook.addEventListener('submit', (e) => {
-    e.preventDefault();
+    e.preventDefault(); 
     saveform1Data();
 
     console.log(form1data)
-    postRequest('https:;//s8-ims-api.herokuapp.com/api/logbooks/', form1data)
+    postRequest(`logbooks/${id}`, form1data) //Post request 
     .then(res => {
       console.log(res)
-      //console.warn('saved', {form1data});
     })
-   
+   //loogbook refreshes on submit
     document.forms[0].reset();
 
 })
-// $review.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     saveform2Data();
 
-//     console.log(form2data)
-//     postRequest('https:;//s8-ims-api.herokuapp.com/api/logbooks/', form2data)
-//     .then(res => {
-//        console.log(res)
-//     })
-//     document.forms[0].reset();
-// })
 
 
 
